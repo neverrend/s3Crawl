b1b06d33ec5b47df262c25a0ecd3e0459c1fc1a2
nrubio <nrubio@cyberninjas.com>
2019-07-26 17:25:01 -0400

    Added command line info/interfacing

--- s3crawl.py
+++ s3crawl.py
@@ -7,35 +7,48 @@ import urllib.request
 import urllib.parse
 
 
-#if len(sys.argv) < 2:
-#    sys.exit(0)
-
-# Replace the URL here. Make sure &continuation-token= is at the end
-url = "http://<URL HERE>/?list-type=2&continuation-token="
-
-# Enter the first token
-contToken = urllib.parse.quote("")
-
-# Put the name of the key you are looking for here
-contReg = re.compile("")
-
-# Dont change this. This finds the next continuation token
-contKey = re.compile("(?:<NextContinuationToken>)(.+)(?:<\/NextContinuationToken>)")
-
-# For Counts
-x = 0
-
-while True:
-    req = urllib.request.urlopen(url+contToken)
-    res = req.read().decode("utf-8")
-
-    found = contReg.search(res)
-    if found:
-        print("Key Found: " + url + contToken + "\n")
+def main():
+    if len(sys.argv) < 3:
+        print(usage())
         sys.exit(0)
-   
-    newContTok = urllib.parse.quote(contKey.search(res).group(1))
-    contToken = newContTok
-    
-    x += 1
-    print("New Token! #{} {}".format(x, contToken))
+
+    url = sys.argv[1]
+    GETparams= "?list-type=2&continuation-token="
+    contToken = urllib.parse.quote(sys.argv[2])
+    contReg = re.compile(sys.argv[3])
+
+    # Dont change this. This finds the next continuation token
+    contKey = re.compile("(?:<NextContinuationToken>)(.+)(?:<\/NextContinuationToken>)")
+
+    # For Counts
+    x = 0
+    space = " "
+
+    #print(url+GETparams+contToken)
+
+    print("\nLooking for the key!")
+
+    while True:
+        req = urllib.request.urlopen(url + GETparams + contToken)
+        res = req.read().decode("utf-8")
+
+        found = contReg.search(res)
+        if found:
+            print(found)
+            if contReg.match(res):
+                print("\nKey Found: " + url + GETparams + contToken + "\n")
+       
+        newContTok = urllib.parse.quote(contKey.search(res).group(1))
+        contToken = newContTok
+        if not newContTok:
+            sys.exit(0)
+         
+        print("\rPlease Wait{}".format("." * x), end="")
+        sys.stdout.flush()
+        x += 1
+
+def usage():
+    print("$ ./s3Crawler.py <url> <first continuation token> <key to look for>")
+
+if __name__ == "__main__":
+    main()
